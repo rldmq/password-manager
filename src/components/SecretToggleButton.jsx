6dec7a1fb1eff8ebf3d71ca027101d4ea9057883
rldmq <76823660+rldmq@users.noreply.github.com>
2024-10-16 23:22:31 -0400

    PWM-0020
    Closes #33
    
    Fix issue with signup page passwordless toggle going infront of the menu.

--- src/components/SecretToggleButton.jsx
+++ src/components/SecretToggleButton.jsx
@@ -17,11 +17,28 @@ export default function SecretToggleButton({ inputId }){
         document.getElementById(inputId).setAttribute('type', 'password')
     }
 
+    React.useEffect(()=>{
+        const els = document.querySelectorAll('.password__btn_show')
+
+        els.forEach(el => {
+            const disableRightClick = el.addEventListener('contextmenu', (event) => event.preventDefault())
+            
+            // Not sure if this works as a cleanup function
+            return () => els.forEach(el => {
+                el.removeEventListener('contextmenu', disableRightClick)
+            })
+        })
+
+    },[])
+
+
     return (
         <button
         className='password__btn_show'  
         onMouseDown={() => handleShowSecret(inputId)}
         onMouseUp={() => handleHideSecret(inputId)}
+        onTouchStart={() => handleShowSecret(inputId)}
+        onTouchEnd={() => handleHideSecret(inputId)}
         onClick={e => e.preventDefault()}
         >
             {symbol}
