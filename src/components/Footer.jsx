98c505045d5a0e9cf8570e7921188f2334db414c
rldmq <76823660+rldmq@users.noreply.github.com>
2024-10-21 15:35:50 -0400

    PWM-0028
    Closes #46
    
    Add Contact page links to footer and sidebar.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer(){
+
+    const currentYear = new Date(Date.now()).getFullYear()
+
     return (
     <footer className='footer'>
-        &copy; FDN
+        &copy; FDN {currentYear}
+        <Link to='/contact'>Contact Us</Link>
     </footer>
     )
 }
